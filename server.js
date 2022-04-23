826e954d61cd57e38e31386f74be11c8b791e407
John-Pena <jmp1495@gmail.com>
2022-04-23 11:07:10 -0500

    server responding and displaying both html files with css and JS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,6 +10,13 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // insert api routes here
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server now on port ${PORT}`);
