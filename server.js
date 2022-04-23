e13187b03c0990e1c7a2bd5fdc9d99dd8a10803b
John-Pena <jmp1495@gmail.com>
2022-04-23 11:45:38 -0500

    modularized html routes in it's own folder and file

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const db = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const htmlRoutes = require('./routes');
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -10,13 +12,10 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // insert api routes here
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/notes.html'));
+app.get('/api/notes', (req, res) => {
+  
 });
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server now on port ${PORT}`);
