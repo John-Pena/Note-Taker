bae4dc8ffcfce7af78bb591e7d0bd0fe9e8155ae
John-Pena <jmp1495@gmail.com>
2022-04-23 10:28:28 -0500

    added git ignore and created server

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// insert api routes here
+
+app.listen(PORT, () => {
+  console.log(`Server now on port ${PORT}`);
+});
